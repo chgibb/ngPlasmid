67f861c1c1b25f9a1acd6d9ece0d7891092af8cb
Chris Gibb <chris.gibb@outlook.com>
2017-10-15 15:36:10 -0400

    initial commit

---
+++ referenceCompiler/index.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+
+const ngcompile = require('@chgibb/ng-node-compile');
+
+let args = process.argv.slice(2);
+
+function compile() : void
+{
+    if(!ngcompile.prototype.envReady)
+    {
+        setTimeout(function(){
+            compile();
+        },100);
+    }
+    let ngEnvironment = new ngcompile([{name : "app", path : "@chgibb/angularplasmid"}]);
+    let input = fs.readFileSync(args[0]).toString();
+    fs.writeFileSync(
+        "result.svg",
+        ngEnvironment.$compile(input)()
+    );
+}
+setTimeout(function(){
+    compile();
+},10);
