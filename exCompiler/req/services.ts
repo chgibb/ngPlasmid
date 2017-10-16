ff1b5669e34a758ceae8c640dde4a1a9242e7d6b
Chris Gibb <chris.gibb@outlook.com>
2017-10-16 09:08:20 -0400

    ported elementScaleLabel

--- exCompiler/req/services.ts
+++ exCompiler/req/services.ts
@@ -17,6 +17,13 @@ export interface Arrow
     angle : number;
 }
 
+export interface Label
+{
+    x : number;
+    y : number;
+    text : string;
+}
+
 export function polarToCartesian(
     centerX : number,
     centerY : number,
@@ -183,4 +190,39 @@ export function pathScale(
     }
     d = d || "M 0,0";
     return d;    
+}
+
+export function elementScaleLabels(
+    x : number,
+    y : number,
+    radius : number,
+    interval : number,
+    total : number
+) : Array<Label> {
+    x = Number(x);
+    y = Number(y);
+    radius = Number(radius);
+    interval = Number(interval);
+    total = Number(total);
+    
+    let alpha : number;
+    let sin : number
+    let cos : number 
+    let i : number;
+    let numTicks : number = Number(interval) > 0 ? Number(total) / Number(interval) : 0;
+    let beta : number = 2 * Math.PI / numTicks;
+    let precision : number = -1;
+    let labelArr : Array<Label> = new Array<Label>();
+    
+    for (i = 0; i < numTicks; i += 1) {
+        alpha = beta * i - Math.PI / 2;
+        cos = Math.cos(alpha);
+        sin = Math.sin(alpha);
+        labelArr.push({
+            x : (<number>(<any>Math).round10((x + (radius * cos)), precision)),
+            y : (<number>(<any>Math).round10((y + (radius * sin)), precision)),
+            text : (<any>(interval * i))
+        });
+    }
+    return labelArr;
 }
