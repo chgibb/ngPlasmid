8c08ae8e724f3a6dc170afe89c693e8183c9454a
Chris Gibb <chris.gibb@outlook.com>
2017-10-16 08:50:45 -0400

    ported pathDonut

--- exCompiler/req/services.ts
+++ exCompiler/req/services.ts
@@ -4,16 +4,55 @@ export interface Point
     y : number;
 }
 
-export function polarToCartesion(
+export interface Ring
+{
+    start : Point;
+    end : Point;
+}
+
+export function polarToCartesian(
     centerX : number,
     centerY : number,
     radius : number,
     angleInDegrees : number
-) : Point
-{
+) : Point {
     let angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
     return {
         x : centerX + (radius * Math.cos(angleInRadians)),
         y : centerY + (radius * Math.sin(angleInRadians))
     };
+}
+
+export function pathDonut(
+    x : number,
+    y : number,
+    radius : number,
+    width : number
+) : string {
+    let innerRing : Ring;
+    let outerRing : Ring;
+    let path;
+
+    x = Number(x || 0);
+    y = Number(y || 0);
+    radius = Number(radius || 0);
+    width = Number(width || 0);
+
+    innerRing = {
+        start : polarToCartesian(x, y, radius, 359.99),
+        end : polarToCartesian(x, y, radius, 0)
+    };
+
+    outerRing = {
+        start : polarToCartesian(x, y, radius + width, 359.99),
+        end : polarToCartesian(x, y, radius + width, 0)
+    };
+
+    path = [
+        "M", innerRing.start.x, innerRing.start.y,
+        "A", radius, radius, 0, 1, 0, innerRing.end.x, innerRing.end.y,
+        "M", outerRing.start.x, outerRing.start.y,
+        "A", radius + width, radius + width, 0, 1, 0, outerRing.end.x, outerRing.end.y
+    ].join(" ");
+    return path;
 }
