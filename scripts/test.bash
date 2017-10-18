ca9b8cb2406c0eacc230d34b153f6ed7e65cbf3c
Chris Gibb <chris.gibb@outlook.com>
2017-10-18 18:17:47 -0400

    refactored

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,28 +1,32 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compatibility
 
+function testFile {
+	f=$1
+	printf "Testing $f\n\n"
+
+	printf "With reference compiler:\n"
+	printf "Compile time:\n"
+	time node referenceCompiler/index samples/$f > "$f"Ref.svg
+	printf "Optimization time:\n"
+	./node_modules/.bin/svgo -i "$f"Ref.svg -o "$f"RefO.svg
+
+	printf "With experimental compiler:\n"
+	printf "Compile time:\n"
+	time node exCompiler/index samples/$f > "$f"Ex.svg
+	printf "Optimization time:\n"
+	./node_modules/.bin/svgo -i "$f"Ex.svg -o "$f"ExO.svg
+	printf "\n"
+	node scripts/fileEquality "$f"ExO.svg "$f"RefO.svg
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+}
+
 ./node_modules/.bin/tsc
 if [ $? != 0 ]; then
     printf "tsc failed\n"
 	exit 1
 fi
 
-printf "Testing 2Labels.html\n\n"
-f="2Labels.html"
-
-printf "With reference compiler:\n"
-printf "Compile time:\n"
-time node referenceCompiler/index samples/$f > "$f"Ref.svg
-printf "Optimization time:\n"
-./node_modules/.bin/svgo -i "$f"Ref.svg -o "$f"RefO.svg
-
-printf "With experimental compiler:\n"
-printf "Compile time:\n"
-time node exCompiler/index samples/$f > "$f"Ex.svg
-printf "Optimization time:\n"
-./node_modules/.bin/svgo -i "$f"Ex.svg -o "$f"ExO.svg
-printf "\n"
-node scripts/fileEquality "$f"ExO.svg "$f"RefO.svg
-if [ $? != 0 ]; then
-	exit 1
-fi
+testFile "2Labels.html"
