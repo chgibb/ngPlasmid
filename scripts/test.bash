cffca9bfba58458ab23d722e233f1e51b5df83a1
Chris Gibb <chris.gibb@outlook.com>
2017-10-18 12:13:51 -0400

    compile before test

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compatibility
 
+./node_modules/.bin/tsc
+if [ $? != 0 ]; then
+    printf "tsc failed\n"
+	exit 1
+fi
+
 printf "Testing 2Labels.html\n\n"
 f="2Labels.html"
 
