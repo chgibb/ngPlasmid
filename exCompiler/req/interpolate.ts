731ccf07548c249ccf7ca77625956db2d2adb5e4
Chris Gibb <chris.gibb@outlook.com>
2017-10-21 17:43:07 -0400

    fleshed out inerpolate

--- exCompiler/req/interpolate.ts
+++ exCompiler/req/interpolate.ts
@@ -1,8 +1,17 @@
+export class Token
+{
+    type : "scopeAccessor" | "addition";
+    value : string;
+}
+
 export function interpolate(value : string,$scope : any) : string
 {
     $scope = "";
+    //If the entire attribute value is not a valid inerpolation expression, then simply return the value
     if(value[0] != "{" || value[1] != "{" || value[value.length-1] != "}" || value[value.length-2] != "}")
         return value;
-
+    //Remove the leading {{ and trailing }}
+    let exp = value.substring(2,value.length-2);
+    console.error(exp.split("."));
     return "";
 }
