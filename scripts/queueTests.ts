b27503f1939d605481fd4d46de11c8b9862e8cdf
Chris Gibb <chris.gibb@outlook.com>
2017-11-25 12:19:19 -0500

    added tests

--- scripts/queueTests.ts
+++ scripts/queueTests.ts
@@ -16,5 +16,65 @@ export function queueTests() : Array<TestCase>
         name : "2 Track Markers"
     }));
 
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "trackMarkers.html",
+        jsonFile : undefined,
+        name : "Track Markers"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "2trackScales.html",
+        jsonFile : undefined,
+        name : "2 Track Scales"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "3trackScales.html",
+        jsonFile : undefined,
+        name : "3 Track Scales"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "1MarkerLabel.html",
+        jsonFile : undefined,
+        name : "1 Marker Label"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "HSP70.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: HSP70"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "pBCA-RLuc.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: pBCA-RLuc"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "pBR322.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: pBR322"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "pLVG440.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: pLVG440"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "pPMA43C.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: pPMA43C"
+    }));
+
+    testCases.push(new TestCase(<TestCaseInit>{
+        htmlFile : "pUC19.html",
+        jsonFile : undefined,
+        name : "Angular Plasmid Example: pUC19"
+    }));
+
     return testCases;
 }
