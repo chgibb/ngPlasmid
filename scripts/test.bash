5c6b6af02017ffa28794d8e75ded7c41a13dfc30
Chris Gibb <chris.gibb@outlook.com>
2017-11-26 22:00:43 -0500

    generate protobuf code before starting tests

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compatibility
 
+./scripts/genPBCode.bash
+
 ./node_modules/.bin/tsc
 if [ $? != 0 ]; then
     printf "tsc failed\n"
