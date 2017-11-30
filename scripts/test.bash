71a169d2154cf800ddc44d65c198e2bdbfbac467
Chris Gibb <chris.gibb@outlook.com>
2017-11-30 11:04:23 -0500

    stop building test bundles

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compatibility
 
-./scripts/buildTestBundles.bash
+./scripts/genPBCode.bash
+
+./node_modules/.bin/tsc
 if [ $? != 0 ]; then
-    printf "Failed to build testing bundles\n"
+    printf "tsc failed\n"
 	exit 1
 fi
 
