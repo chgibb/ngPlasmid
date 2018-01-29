a055f1b9d5265d0a7913ae50ecbc58ae5399d818
Chris Gibb <chris.gibb@outlook.com>
2018-01-28 22:09:33 -0500

    started to type module interface

---
+++ lib/ngPlasmid.node.d.ts
@@ -0,0 +1,11 @@
+import * as services from "./services";
+
+export class ngPlasmidNative
+{
+    public polarToCartesian(
+        centerX : number,
+        centerY : number,
+        radius : number,
+        angleInDegrees : number
+    ) : services.Point
+}
