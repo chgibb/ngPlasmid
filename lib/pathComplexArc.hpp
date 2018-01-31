4545f18b949ddca67fb6b42ad177766888a65a6a
Chris Gibb <chris.gibb@outlook.com>
2018-01-31 18:56:57 -0500

    reverted all

--- lib/pathComplexArc.hpp
+++ lib/pathComplexArc.hpp
@@ -11,19 +11,19 @@
 #include "polarToCartesian.hpp"
 namespace ngPlasmid
 {
-    inline const std::string pathComplexArc(
-        long double&x,
-        long double&y,
-        long double&radius,
-        long double&startAngle,
-        long double&endAngle,
-        long double&width,
-        long double&arrowStartWidth,
-        long double&arrowStartLength,
-        long double&arrowStartAngle,
-        long double&arrowEndWidth,
-        long double&arrowEndLength,
-        long double&arrowEndAngle
+    const std::string pathComplexArc(
+        long double x,
+        long double y,
+        long double radius,
+        long double startAngle,
+        long double endAngle,
+        long double width,
+        long double arrowStartWidth,
+        long double arrowStartLength,
+        long double arrowStartAngle,
+        long double arrowEndWidth,
+        long double arrowEndLength,
+        long double arrowEndAngle
     ) {
         //std::stringstream ss;
         //std::string ss;
--- lib/polarToCartesian.hpp
+++ lib/polarToCartesian.hpp
@@ -5,11 +5,11 @@
 namespace ngPlasmid
 {
     const long double JS_PI = 3.141592653589793;
-    inline Point polarToCartesian(
-        long double&centerX,
-        long double&centerY,
-        long double&radius,
-        long double&angleInDegrees
+    Point polarToCartesian(
+        long double centerX,
+        long double centerY,
+        long double radius,
+        long double angleInDegrees
     ) {
         Point res;
         long double angleInRadians = (angleInDegrees - 90) * JS_PI / 180.0;
