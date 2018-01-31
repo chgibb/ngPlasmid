ba43a29f25cc5003f0d40ec0d924b1f0db911e80
Chris Gibb <chris.gibb@outlook.com>
2018-01-31 18:58:00 -0500

    mark inline and added explicit scoping

--- lib/polarToCartesian.hpp
+++ lib/polarToCartesian.hpp
@@ -5,13 +5,13 @@
 namespace ngPlasmid
 {
     const long double JS_PI = 3.141592653589793;
-    Point polarToCartesian(
+    inline ngPlasmid::Point polarToCartesian(
         long double centerX,
         long double centerY,
         long double radius,
         long double angleInDegrees
     ) {
-        Point res;
+        ngPlasmid::Point res;
         long double angleInRadians = (angleInDegrees - 90) * JS_PI / 180.0;
         res.x = centerX + (radius * std::cos(angleInRadians));
         res.y = centerY + (radius * std::sin(angleInRadians));
