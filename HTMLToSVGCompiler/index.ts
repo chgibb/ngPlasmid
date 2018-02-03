95a3459cbd7a8f5b931c2d89247927038ffacfb4
Chris Gibb <chris.gibb@outlook.com>
2018-02-03 16:26:27 -0500

    call batch method in batched mode

--- HTMLToSVGCompiler/index.ts
+++ HTMLToSVGCompiler/index.ts
@@ -8,14 +8,13 @@ import * as html from "./../lib/html";
 import * as directives from "./../lib//directives";
 
 (async function(){
-    if(args[args.length-1] == "batched")
-    {}
-        
     let nodes = await html.loadFromString(fs.readFileSync(args[0]).toString());
 
     let plasmid = new directives.Plasmid();
+
     if(args[1] && args[1] != "batched")
         plasmid.$scope = JSON.parse(fs.readFileSync(args[1]).toString());
+
     for(let i = 0; i != nodes.length; ++ i)
     {
         if(nodes[i].name == "plasmid")
@@ -24,5 +23,11 @@ import * as directives from "./../lib//directives";
             break;
         }
     }
+
+    if(args[args.length-1] == "batched")
+    {
+        plasmid.batchGenerateSVGPaths();    
+    }
+
     console.log(plasmid.renderStart() + plasmid.renderEnd());
 })();
--- PBToSVGCompiler/index.ts
+++ PBToSVGCompiler/index.ts
@@ -9,15 +9,20 @@ import * as directives from "./../lib//directives";
 import {plasmidToPB} from "./../lib/directiveToPB";
 import * as pbDirectives from "./../lib/pb/node";
 
-if(args[args.length-1] == "batched")
-{}
-
 let plasmid = new directives.Plasmid();
+
 if(args[1] && args[1] != "native")
     plasmid.$scope = JSON.parse(fs.readFileSync(args[1]).toString());
 
 let buffer = fs.readFileSync(args[0]);
+
 plasmid.fromNode<any>(
     pbDirectives.Node.decode(buffer)
 );
+
+if(args[args.length-1] == "batched")
+{
+    plasmid.batchGenerateSVGPaths();
+}
+
 console.log(plasmid.renderStart() + plasmid.renderEnd());
