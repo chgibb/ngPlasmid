22707768580f8080b9e998b83a3530acdfda24ef
Chris Gibb <chris.gibb@outlook.com>
2018-04-24 09:43:53 -0400

    generalized style parsing

--- lib/parseFontSize.ts
+++ lib/parseFontSize.ts
@@ -1,3 +1,5 @@
+import {parseStyle} from "./parseStyle";
+
 /**
  * Parses the value of font-size:valuepx out of the CSS string style.
  * font-size must be specified in px and not %
@@ -9,44 +11,17 @@
 export function parseFontSize(style : string) : number
 {
     //https://github.com/vixis/angularplasmid/blob/master/src/js/directives.js#L973
-    let str = "";
-    let res = 0;
-    let gettingValue = false;
-    for(let i = 0; i != style.length; ++i)
-    {
-        //trying to parse out "font-size:"
-        if(!gettingValue)
-        {
-            if(style[i] == ":")
-            {
-                if(str == "font-size")
-                {
-                    gettingValue = true;
-                    str = "";
-                    continue;
-                }
-            }
-            if(style[i] != " ")
-            {
-                str += style[i];
-                continue;
-            }
-        }
-        //getting actual font-size value. Stop after reaching p
-        if(gettingValue)
-        {
-            if(style[i] != " " && style[i] != "p")
-            {
-                str += style[i];
-                continue;
-            }
-            if(style[i] == "p")
-            {
-                return parseInt(str);
-            }
 
+    let styles = parseStyle(style);
+    if(!styles)
+        return 0;
+    for(let i in styles)
+    {
+        if(i == "font-size")
+        {
+            return parseInt(styles[i]);
         }
     }
+    return 0;
 
-    return res;
 }
