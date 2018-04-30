88bd85fcb83101ba557dcb6188ffe7b2b5f8d3a2
Chris Gibb <chris.gibb@outlook.com>
2018-04-29 22:13:19 -0400

    initial tracklabel to canvas

--- lib/canvas/trackLabel.ts
+++ lib/canvas/trackLabel.ts
@@ -2,8 +2,26 @@ import {TrackLabel} from "./../directives";
 import {parseStyle} from "./../parseStyle";
 import {drawSVGarcOnCanvas} from "./utils";
 
-export function trackLabelToCanvas(track : TrackLabel,ctx : CanvasRenderingContext2D) : void
+export function trackLabelToCanvas(label : TrackLabel,ctx : CanvasRenderingContext2D) : void
 {
-    track;
-    ctx;
+    label.interpolateAttributes();
+
+    ctx.fillStyle = "#000";
+    ctx.strokeStyle = "rgba(0,0,0,0)";
+
+    let style = parseStyle(label.labelstyle);    
+
+    let weight = style ? (style["font-weight"] ? style["font-weight"] : "") : "";
+    let size = style ? (style["font-size"] ? style["font-size"] : "") : "";
+    let font = "sans-serif";
+    let text = label.text ? label.text : "";
+
+    ctx.font = " " + weight + " " + size + " " + font;
+    ctx.textBaseline = "middle";
+    let measure : TextMetrics = ctx.measureText(text);
+    let x = label.track.center.x+label.hadjust - (measure.width/2);
+    let y = label.track.center.y+label.vadjust;
+
+    ctx.fillText(text,x,y);
+    ctx.strokeText(text,x,y);
 }
