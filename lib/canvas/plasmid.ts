c0bd7ef6ab55d0eaa21a566d48974fc81b3b9924
Chris Gibb <chris.gibb@outlook.com>
2018-06-18 22:09:02 -0400

    interpolate on start

--- lib/canvas/plasmid.ts
+++ lib/canvas/plasmid.ts
@@ -4,6 +4,7 @@ import {Plasmid} from "./../directives";
 
 export function plasmidToCanvas(plasmid : Plasmid,ctx : CanvasRenderingContext2D) : void
 {
+    plasmid.interpolateAttributes();
     for(let i = 0; i != plasmid.tracks.length; ++i)
     {
         plasmid.tracks[i].toCanvas(ctx);
