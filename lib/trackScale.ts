add6110e4377335fd8f329b0825f488d6bf6ba21
Chris Gibb <chris.gibb@outlook.com>
2019-07-03 19:37:56 -0400

    fixed imports

--- lib/trackScale.ts
+++ lib/trackScale.ts
@@ -3,8 +3,8 @@ import { PlasmidTrack } from "./plasmidTrack";
 import { interpolate } from "./interpolate";
 import { GenericNode } from "./genericNode";
 import { trackScaleToCanvas } from "./canvas/trackScale";
-import { pathScale } from "./services/svg/pathScalecale";
-import { elementScaleLabels } from "./services/svg/elementScaleLabelsbels";
+import { pathScale } from "./services/svg/pathScale";
+import { elementScaleLabels } from "./services/svg/elementScaleLabels";
 
 /**
  * This element provides labels and tickmarks for the track.
